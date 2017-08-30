26e9af761dd0f83370cff20b2c258bd912516d68
Alexander Kurtz <alexander@kurtz.be>
2017-08-30 19:58:11 +0200

    scripts/speedtest.sh: Remove the types; they are auto-detected now.

--- scripts/speedtest.sh
+++ scripts/speedtest.sh
@@ -30,37 +30,37 @@ echo "Spawning a simple echo daemon on [::1]:4242 *with* aflwrapper with lots of
 	--address '::1' \
 	--port '4242' \
 	\
-	--set 'allow4,uint32_t:00,subnet4:1.0.0.0/24' \
-	--set 'allow4,uint32_t:01,subnet4:1.0.0.1/24' \
-	--set 'allow4,uint32_t:02,subnet4:1.0.0.2/24' \
-	--set 'allow4,uint32_t:03,subnet4:1.0.0.3/24' \
-	--set 'allow4,uint32_t:04,subnet4:1.0.0.4/24' \
-	--set 'allow4,uint32_t:05,subnet4:1.0.0.5/24' \
-	--set 'allow4,uint32_t:06,subnet4:1.0.0.6/24' \
-	--set 'allow4,uint32_t:07,subnet4:1.0.0.7/24' \
-	--set 'allow4,uint32_t:08,subnet4:1.0.0.8/24' \
-	--set 'allow4,uint32_t:09,subnet4:1.0.0.9/24' \
-	--set 'allow4,uint32_t:10,subnet4:1.0.0.10/24' \
-	--set 'allow4,uint32_t:11,subnet4:1.0.0.11/24' \
-	--set 'allow4,uint32_t:12,subnet4:1.0.0.12/24' \
-	--set 'allow4,uint32_t:13,subnet4:1.0.0.13/24' \
-	--set 'allow4,uint32_t:14,subnet4:127.0.0.1/24' \
+	--set 'allow4,00,1.0.0.0/24' \
+	--set 'allow4,01,1.0.0.1/24' \
+	--set 'allow4,02,1.0.0.2/24' \
+	--set 'allow4,03,1.0.0.3/24' \
+	--set 'allow4,04,1.0.0.4/24' \
+	--set 'allow4,05,1.0.0.5/24' \
+	--set 'allow4,06,1.0.0.6/24' \
+	--set 'allow4,07,1.0.0.7/24' \
+	--set 'allow4,08,1.0.0.8/24' \
+	--set 'allow4,09,1.0.0.9/24' \
+	--set 'allow4,10,1.0.0.10/24' \
+	--set 'allow4,11,1.0.0.11/24' \
+	--set 'allow4,12,1.0.0.12/24' \
+	--set 'allow4,13,1.0.0.13/24' \
+	--set 'allow4,14,127.0.0.1/24' \
 	\
-	--set 'allow6,uint32_t:00,subnet6:1::0/72' \
-	--set 'allow6,uint32_t:01,subnet6:1::1/72' \
-	--set 'allow6,uint32_t:02,subnet6:1::2/72' \
-	--set 'allow6,uint32_t:03,subnet6:1::3/72' \
-	--set 'allow6,uint32_t:04,subnet6:1::4/72' \
-	--set 'allow6,uint32_t:05,subnet6:1::5/72' \
-	--set 'allow6,uint32_t:06,subnet6:1::6/72' \
-	--set 'allow6,uint32_t:07,subnet6:1::7/72' \
-	--set 'allow6,uint32_t:08,subnet6:1::8/72' \
-	--set 'allow6,uint32_t:09,subnet6:1::9/72' \
-	--set 'allow6,uint32_t:10,subnet6:1::10/72' \
-	--set 'allow6,uint32_t:11,subnet6:1::11/72' \
-	--set 'allow6,uint32_t:12,subnet6:1::12/72' \
-	--set 'allow6,uint32_t:13,subnet6:1::13/72' \
-	--set 'allow6,uint32_t:14,subnet6:::1/128' \
+	--set 'allow6,00,1::0/72' \
+	--set 'allow6,01,1::1/72' \
+	--set 'allow6,02,1::2/72' \
+	--set 'allow6,03,1::3/72' \
+	--set 'allow6,04,1::4/72' \
+	--set 'allow6,05,1::5/72' \
+	--set 'allow6,06,1::6/72' \
+	--set 'allow6,07,1::7/72' \
+	--set 'allow6,08,1::8/72' \
+	--set 'allow6,09,1::9/72' \
+	--set 'allow6,10,1::10/72' \
+	--set 'allow6,11,1::11/72' \
+	--set 'allow6,12,1::12/72' \
+	--set 'allow6,13,1::13/72' \
+	--set 'allow6,14,::1/128' \
 	\
 	ebpf/whitelist.c \
 	systemd-socket-activate --accept --inetd cat 1>/dev/null 2>/dev/null & daemon="${!}"
